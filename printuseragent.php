c7b9e259aff33a824b337dbeae6dde53bab101d7
undefinedmode <github@undefinedmode.net>
2018-04-25 12:00:43 +0200

    Add files via upload
    PHP print User-agent info

---
+++ printuseragent.php
@@ -0,0 +1,5 @@
+<?php
+// Obviously this is for testing purposes only, do not put on any live environments
+   $useragent = $_SERVER ['HTTP_USER_AGENT'];
+   echo "<b>Your User Agent is</b>: " . $useragent;
+?>
