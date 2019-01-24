2f6ac626eb1158fb66fe06c5e4aac89ee895f2be
undefinedmode <github@undefinedmode.net>
2019-01-24 20:19:20 +0100

    unzip.php
    PHP file that extracts a ZIP file

---
+++ unzip.php
@@ -0,0 +1,19 @@
+<?php
+// extract a zip file from PHP. Source: https://stackoverflow.com/questions/8889025/unzip-a-file-with-php
+// assuming file.zip is in the same directory as the executing script.
+$file = 'unzip.zip';
+
+// get the absolute path to $file
+$path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === TRUE) {
+  // extract it to the path we determined above
+  $zip->extractTo($path);
+  $zip->close();
+  echo "WOOT! $file extracted to $path";
+} else {
+  echo "Doh! I couldn't open $file";
+}
+?>
